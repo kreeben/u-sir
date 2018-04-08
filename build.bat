6e808b0ec3b7f81a5b1960c5f2539f7cd3a7dfc2
Marcus Lager <marcuslager@gmail.com>
2018-04-08 06:25:49 +0200

    convencience bat files

---
+++ build.bat
@@ -0,0 +1,4 @@
+dotnet build src/sir.httpserver --configuration Release
+dotnet build src/sir.json --configuration Release
+dotnet build src/sir.index --configuration Release
+dotnet build src/sir.store --configuration Release
---
+++ start.bat
@@ -0,0 +1,1 @@
+dotnet run --project src\Sir.HttpServer
